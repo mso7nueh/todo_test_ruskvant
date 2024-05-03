d95747f6c47e0dfe319ab68173bdd5da06647400
mso7nueh <mso7nueh@mail.ru>
2024-05-04 00:51:51 +0300

    fix snack bar

--- lib/features/main/presentation/pages/todo_form_screen.dart
+++ lib/features/main/presentation/pages/todo_form_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_test_ruskvant/features/main/models/todo_model.dart';
 import 'package:todo_test_ruskvant/features/main/presentation/getx/todo_controller.dart';
@@ -29,6 +28,39 @@ class _TodoFormScreenState extends State<TodoFormScreen> {
   }
 
   @override
+  void didChangeDependencies() {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    super.didChangeDependencies();
+  }
+
+  void addNewTodo() {
+    todoController.addTodoItem(
+      completed: isCompleted,
+      title: textEditingController.text,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text('Task added successfully!'),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+        label: 'Undo',
+        onPressed: () {
+          todoController.removeLastTask();
+        },
+      ),
+    ));
+    Navigator.pop(context);
+  }
+
+  void updateTodo() {
+    todoController.changeTodoItem(
+      todoItem: widget.todoModel!,
+      completed: isCompleted,
+      title: textEditingController.text,
+    );
+    Navigator.pop(context);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -44,7 +76,7 @@ class _TodoFormScreenState extends State<TodoFormScreen> {
               controller: textEditingController,
             ),
             const SizedBox(height: 16),
-            CupertinoSwitch(
+            Switch(
               value: isCompleted,
               onChanged: (value) {
                 setState(() {
@@ -56,31 +88,7 @@ class _TodoFormScreenState extends State<TodoFormScreen> {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {
-                  if (widget.todoModel != null) {
-                    todoController.changeTodoItem(
-                      todoItem: widget.todoModel!,
-                      completed: isCompleted,
-                      title: textEditingController.text,
-                    );
-                  } else {
-                    todoController.addTodoItem(
-                      completed: isCompleted,
-                      title: textEditingController.text,
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: const Text('Task added successfully!'),
-                      duration: const Duration(seconds: 3),
-                      action: SnackBarAction(
-                        label: 'Undo',
-                        onPressed: () {
-                          todoController.removeLastTask();
-                        },
-                      ),
-                    ));
-                  }
-                  Navigator.pop(context);
-                },
+                onPressed: widget.todoModel != null ? updateTodo : addNewTodo,
                 child: Text(widget.todoModel != null ? 'Save' : 'Add'),
               ),
             ),
